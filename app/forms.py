767c87d48b738c71e614171d6e25fb080dd73b8f
sushantahujaa <sushantahuja35@gmail.com>
2020-03-13 07:03:54 -0400

    Adds the login and registeration form for the volunteer

--- app/forms.py
+++ app/forms.py
@@ -25,6 +25,26 @@ class RegistrationForm(FlaskForm):
     #         raise ValidationError('Please use a different name.')
 
     def validate_email(self, email):
+        organization = Organization.query.filter_by(email=email.data).first()
+        if organization is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+class VolunteerLoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+
+class VolunteerRegistrationForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
         volunteer = Volunteer.query.filter_by(email=email.data).first()
         if volunteer is not None:
             raise ValidationError('Please use a different email address.')
