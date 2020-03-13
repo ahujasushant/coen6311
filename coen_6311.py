b7dce7e1ae9c01dfcd4c9cfadd2bd8e9eb6b30ef
sushantahujaa <sushantahuja35@gmail.com>
2020-03-13 07:00:46 -0400

    Adds the migration for Posts table

--- coen_6311.py
+++ coen_6311.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import Volunteer, Organization
+from app.models import Volunteer, Organization, Post
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Volunteer': Volunteer, 'Organization': Organization}
+    return {'db': db, 'Volunteer': Volunteer, 'Organization': Organization, 'Post': Post}
