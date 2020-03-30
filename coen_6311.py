23143d63df4608c37235880e3ada5aebf6d60bf5
sushantahujaa <sushantahuja35@gmail.com>
2020-03-30 16:22:06 -0400

    Updates the file for importing the History model

--- coen_6311.py
+++ coen_6311.py
@@ -1,7 +1,7 @@
-from app import __init__, db
-from app.models import Volunteer, Organization, Post
+from coen_6311 import __init__, db
+from coen_6311.models import Volunteer, Organization, Post, History
 
 
 @__init__.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Volunteer': Volunteer, 'Organization': Organization, 'Post': Post}
+    return {'db': db, 'Volunteer': Volunteer, 'Organization': Organization, 'Post': Post, 'History': History}
