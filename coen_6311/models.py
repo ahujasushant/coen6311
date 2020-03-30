9a7fba35aeb3f52f2f42d08eb638385345afbb20
sushantahujaa <sushantahuja35@gmail.com>
2020-03-30 16:23:48 -0400

    Updates the models file for adding the association betweeen the models and adds the history model to the project

--- coen_6311/models.py
+++ coen_6311/models.py
@@ -9,19 +9,25 @@ class Volunteer(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    dob = db.Column(db.String(128))
-    street_name = db.Column(db.String(128))
-    street_number = db.Column(db.String(128))
-    post_code = db.Column(db.String(128))
-    country = db.Column(db.String(128))
-    phone = db.Column(db.String(128))
-    days_of_availability = db.Column(db.String(128))
-    areas_of_interest = db.Column(db.String(128))
+    dob = db.Column(db.String(128), default='')
+    street_name = db.Column(db.String(128), default='')
+    street_number = db.Column(db.String(128), default='')
+    post_code = db.Column(db.String(128), default='')
+    state = db.Column(db.String(128), default='')
+    country = db.Column(db.String(128), default='')
+    phone = db.Column(db.String(128), default='')
+    days_of_availability = db.Column(db.String(128), default='')
+    areas_of_interest = db.Column(db.String(128), default='')
     password_hash = db.Column(db.String(128))
-
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    history = db.relationship('History', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<Volunteer {}>'.format(self.name)
+
+    def location(self):
+        if self.street_number and self.street_name and self.state and self.country and self.post_code:
+            return self.street_number + ',' + self.street_name + ',' + self.state + ',' + self.country + ',' + self.post_code
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -30,9 +36,13 @@ class Volunteer(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
-@login.user_loader
-def load_user(id):
-    return Volunteer.query.get(int(id))
+class History(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(64), index=True)
+    volunteer_id = db.Column(db.Integer, db.ForeignKey('volunteer.id'))
+
+    def __repr__(self):
+        return '<History {}>'.format(self.location)
 
 
 class Organization(db.Model):
@@ -41,6 +51,8 @@ class Organization(db.Model):
     founded_in = db.Column(db.String(120), index=True, unique=True)
     official_email = db.Column(db.String(128))
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
 
     def __repr__(self):
         return '<Organization {}>'.format(self.org_name)
@@ -56,6 +68,9 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(64))
     location = db.Column(db.String(120))
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+    volunteers = db.relationship('Volunteer', backref='author', lazy='dynamic')
+
 
     def __repr__(self):
         return '<Post {}>'.format(self.description, self.location)
