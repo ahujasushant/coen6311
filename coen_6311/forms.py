cbefe6fb7e02dd07af22c3c93fbae161dedbaeb0
sushantahujaa <sushantahuja35@gmail.com>
2020-03-30 16:23:10 -0400

    Adds the form for editing the volunteer and organization profile

--- coen_6311/forms.py
+++ coen_6311/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms  import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from coen_6311.models import Volunteer, Organization, Post
 
@@ -31,9 +31,32 @@ class VolunteerLoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
 
+
+class VolunteerEditForm(FlaskForm):
+    name = StringField('Name')
+    email = StringField('Email')
+    dob = DateField('Date of Birth')
+    street_name = StringField('Street Name')
+    street_number = StringField('Street Number')
+    state = StringField('State')
+    post_code = StringField('Postal Code')
+    country = StringField('Country')
+    phone = StringField('Contact Number')
+    submit = SubmitField('Update Details')
+
+
+class OrganizationEditForm(FlaskForm):
+    org_name = StringField('Organization Name', validators=[DataRequired()])
+    official_email = StringField('Official Organization Email', validators=[DataRequired()])
+    founded_in = DateField('Organization Founded in', format='%m/%d/%Y', validators=[DataRequired()])
+    submit = SubmitField('Update Details')
+
+
 class PostForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
+    # o = Organization.query.get(cur)
+
     submit = SubmitField('Create Post')
 
 
