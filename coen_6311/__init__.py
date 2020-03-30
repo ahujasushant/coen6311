6ee769a9ea213b534167a13fcafd596ce514f53a
sushantahujaa <sushantahuja35@gmail.com>
2020-03-30 16:22:42 -0400

    Updates the init.py file for importing the models

--- coen_6311/__init__.py
+++ coen_6311/__init__.py
@@ -1,14 +1,24 @@
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
+
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from coen_6311 import routes, models
+
+from coen_6311 import models
+from coen_6311.models import *
+from coen_6311.routes import *
+
+
+if __name__ == '__main__':
+    app.run()
